7f6b2d8fa4da76fb6bbed87d12149e832fccc8d3
ayushpaharia <ayush.paharia.18@gmail.com>
2021-07-02 18:28:21 +0530

    switched to typescript, added apollo initialization

---
+++ lib/withApollo.tsx
@@ -0,0 +1,34 @@
+import {
+  ApolloClient,
+  ApolloProvider,
+  DefaultOptions,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import { useRouter } from "next/dist/client/router";
+import nextWithApollo from "next-with-apollo";
+
+const withApollo = nextWithApollo(
+  ({ initialState, headers }) => {
+    return new ApolloClient({
+      ssrMode: typeof window === "undefined",
+      link: new HttpLink({
+        uri: "https://rickandmortyapi.com/graphql",
+      }),
+      headers: {
+        ...(headers as Record<string, string>),
+      },
+      cache: new InMemoryCache().restore(initialState || {}),
+    });
+  },
+  {
+    render: ({ Page, props }) => {
+      const router = useRouter();
+      return (
+        <ApolloProvider client={props.apollo}>
+          <Page {...props} {...router} />
+        </ApolloProvider>
+      );
+    },
+  }
+);
