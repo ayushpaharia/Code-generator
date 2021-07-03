a187be15a7cec746a88fc3f442e0f2aa1ab430ab
ayushpaharia <ayush.paharia.18@gmail.com>
2021-07-03 05:47:20 +0530

    worked on /characters/[id] page

--- pages/characters/[id].tsx
+++ pages/characters/[id].tsx
@@ -2,17 +2,33 @@ import { get } from "lodash";
 import { getDataFromTree } from "@apollo/client/react/ssr";
 import withApollo from "../../lib/withApollo";
 import { useCharacterQuery } from "../../generated";
+import Image from "next/image";
 
 function SingleCharacterPage({ query }) {
   const characterId = get(query, "id");
 
-  const { data } = useCharacterQuery({
+  const { data, loading } = useCharacterQuery({
     variables: {
       id: characterId,
     },
   });
+  if (loading) return <div>loading...</div>;
+  const {
+    name,
+    species,
+    image,
+    origin: { dimension },
+  } = data.character;
+  console.log(data.character);
 
-  return <div>{JSON.stringify(data)}</div>;
+  return (
+    <div>
+      <Image alt={name} src={image} width="100px" height="100px" />
+      <h3>
+        {species} {name} from {dimension != null ? dimension : "Unknown"}
+      </h3>
+    </div>
+  );
 }
 
 export default withApollo(SingleCharacterPage, { getDataFromTree });
